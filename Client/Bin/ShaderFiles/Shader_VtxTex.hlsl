72b941c880c51f2c35eab5f583a5588278c02a7c
KimSeonMin00 <103050847+KimSeonMin00@users.noreply.github.com>
2022-05-30 23:57:39 +0900

    Fix : Shader

--- Client/Bin/ShaderFiles/Shader_VtxTex.hlsl
+++ Client/Bin/ShaderFiles/Shader_VtxTex.hlsl
@@ -58,7 +58,10 @@ PS_OUT PS_MAIN_RECT(PS_IN In)
 {
 	PS_OUT		Out = (PS_OUT)0;
 
-	Out.vColor = g_DiffuseTexture.Sample(DefaultSampler, In.vTexUV);
+	vector		vMtrlDiffuse = g_DiffuseTexture.Sample(DefaultSampler, In.vTexUV);
+	vector    vGreen = vector (0.f, 0.8f, 0.f, 1.f);
+
+	Out.vColor = vGreen * vMtrlDiffuse;
 
 	return Out;
 }
