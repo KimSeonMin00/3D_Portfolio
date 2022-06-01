b31f33205f196d1f2d386a11271cfce1d001a8bc
KimSeonMin00 <103050847+KimSeonMin00@users.noreply.github.com>
2022-06-01 19:22:36 +0900

    Feat : FileDialog

--- Client/Private/ImguiManager.cpp
+++ Client/Private/ImguiManager.cpp
@@ -138,6 +138,70 @@ void CImguiManager::Tick(_double TimeDelta)
 		Safe_Release(m_pTransform);
 	}
 
+	if (ImGui::Button("File"))
+	{
+		HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED |
+			COINIT_DISABLE_OLE1DDE);
+		if (SUCCEEDED(hr))
+		{
+			IFileSaveDialog *pFileSave;
+
+			// Create the FileOpenDialog object.
+			hr = CoCreateInstance(CLSID_FileSaveDialog, NULL, CLSCTX_ALL,
+				IID_IFileSaveDialog, reinterpret_cast<void**>(&pFileSave));
+
+			if (SUCCEEDED(hr))
+			{
+
+				DWORD dwFlags;
+
+				hr = pFileSave->GetOptions(&dwFlags);
+
+				if (SUCCEEDED(hr))
+				{
+					COMDLG_FILTERSPEC rgSpec[] =
+					{
+						{ L"DataFiles(*.dat)", L"*.dat" }
+					};
+
+					hr = pFileSave->SetFileTypes(ARRAYSIZE(rgSpec), rgSpec);
+
+					if (SUCCEEDED(hr))
+					{
+						hr = pFileSave->SetDefaultExtension(L"dat");
+
+						if(SUCCEEDED(hr))
+						{
+							// Show the Open dialog box.
+							hr = pFileSave->Show(NULL);
+
+							// Get the file name from the dialog box.
+							if (SUCCEEDED(hr))
+							{
+								IShellItem *pItem;
+								hr = pFileSave->GetResult(&pItem);
+								if (SUCCEEDED(hr))
+								{
+									PWSTR pszFilePath;
+									hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
+									// Display the file name to the user.
+									if (SUCCEEDED(hr))
+									{
+										MessageBoxW(NULL, pszFilePath, L"File Path", MB_OK);
+										CoTaskMemFree(pszFilePath);
+									}
+									pItem->Release();
+								}
+							}
+						}
+					}
+				}
+				pFileSave->Release();
+			}
+			CoUninitialize();
+		}
+	}
+
 	ImGui::End();
 }
 
